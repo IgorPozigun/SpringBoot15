0575519c453774307481c0ee0c80ec97dde2a86e
life with java <pozigun.92@mail.ru>
2022-08-24 15:37:16 +0300

    не отвечает на запросы фронта

--- src/main/java/com/example/SpringBoot2/controller/UserController.java
+++ src/main/java/com/example/SpringBoot2/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/User")
+@RequestMapping("/user")
 public class UserController {
 
     private UserService userService;
--- src/main/java/com/example/SpringBoot2/model/User.java
+++ src/main/java/com/example/SpringBoot2/model/User.java
@@ -9,7 +9,7 @@ import javax.persistence.Table;
 import java.io.Serializable;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 public class User implements Serializable { // объект возводим в байт и передаем его по сети
 
     @Id
